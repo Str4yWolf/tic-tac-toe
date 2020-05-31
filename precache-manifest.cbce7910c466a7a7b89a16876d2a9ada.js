71a5c9b6b0832f54ca3cd45573e1e637b4fec144
Michael Huang <michaelhuang@hotmail.de>
2020-05-31 11:32:32 +0200

    Updates

---
+++ precache-manifest.cbce7910c466a7a7b89a16876d2a9ada.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dcde707f0361797b7c2cc26ee9a4fcf",
+    "url": "/games/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "6cfcd5db7f21e25582d0",
+    "url": "/games/tic-tac-toe/static/css/main.fac38491.chunk.css"
+  },
+  {
+    "revision": "904d7606144bb35c1a7b",
+    "url": "/games/tic-tac-toe/static/js/2.88b226b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/games/tic-tac-toe/static/js/2.88b226b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cfcd5db7f21e25582d0",
+    "url": "/games/tic-tac-toe/static/js/main.b43d492b.chunk.js"
+  },
+  {
+    "revision": "27f93364403c6b4a41e6",
+    "url": "/games/tic-tac-toe/static/js/runtime-main.583ada18.js"
+  }
+]);
