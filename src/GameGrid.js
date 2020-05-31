f507981d1ee69d195160208a8dcaaaf77037ebea
Michael Huang <michaelhuang@hotmail.de>
2020-05-31 11:22:57 +0200

    Finished tic tac toe basic

--- src/GameGrid.js
+++ src/GameGrid.js
@@ -1,67 +1,131 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles.scss';
 import GameTile from './GameTile';
 
-const GameGrid = ({ dims }) => {
-  const initialTiles = ['','','','','','','','',''];
-  const initialTurn = 'x';
-  const initialStatus =  `Ready to play; it's ${initialTurn}'s turn.`;
+const GameGrid = () => {
+  const VALID_SHAPES = ['x', 'o'];
+  const SHAPE1 = VALID_SHAPES[0];
+  const SHAPE2 = VALID_SHAPES[1];
 
-  const solutions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+  const SOLUTIONS = [
+                      [0,1,2],[3,4,5],[6,7,8],
+                      [0,3,6],[1,4,7],[2,5,8],
+                      [0,4,8],[2,4,6]
+                    ];
+
+  const INITIAL_TILES = ['','','','','','','','',''];
+  const INITIAL_TURN = SHAPE1;
+  const INITIAL_STATUS =  `Ready to play; it's ${INITIAL_TURN}'s turn.`;
+
+  const INVALID_MOVE_MESSAGE = 'Invalid move. Please click an empty field.';
+
   
-  const [tiles, setTiles] = useState(initialTiles);
-  const [currentTurn, setCurrentTurn] = useState(initialTurn);
-  const [status, setStatus] = useState(initialStatus);
+  const [tiles, setTiles] = useState(INITIAL_TILES);
+  const [currentTurn, setCurrentTurn] = useState(INITIAL_TURN);
+  const [status, setStatus] = useState(INITIAL_STATUS);
+  const [gameOver, setGameOver] = useState(false);
 
-  const changeTurn = () => setCurrentTurn(currentTurn === 'x' ? 'o' : 'x');
 
-  const playTurn = id => {
-    if (tiles[id] !== '') return setStatus('Invalid move.');
-    let newTiles = tiles.slice();
-    newTiles[id] = currentTurn;
-    setTiles(newTiles);
-    checkGameOver();
-    changeTurn();
+
+  useEffect(() => {
+    if (tiles !== INITIAL_TILES) onTilesChange();
+  }, [tiles]);
+
+
+
+
+  const setCheckedTile = checkedTileIndex => {
+    if (tiles[checkedTileIndex] !== '') return setStatus(INVALID_MOVE_MESSAGE);
+
+    let updatedTiles = tiles.slice();
+    updatedTiles[checkedTileIndex] = currentTurn;
+    setTiles(updatedTiles);
+  } 
+
+
+  const onTilesChange = () => {
+    let winner = checkForWin();
+    if (winner) {
+      setWinner(winner);
+      setGameOver(true);
+    } else {
+      changeTurn();
+    }
   }
 
-  const checkGameOver = id => {
-    let currentShapeTiles = getTilesWithShape();
-    if (solutions.some(s => s.every(t => currentShapeTiles.includes(t)))) {
-      setStatus(`The winner is ${currentTurn}!`);
+
+  const changeTurn = () => {
+    setCurrentTurn(currentTurn === SHAPE1 ? SHAPE2 : SHAPE1);
+    setStatus('');
+  }
+
+
+  const checkForWin = () => {
+    let currentShapeTiles = getTileIndicesWithShape(currentTurn);
+
+    if (SOLUTIONS.some(solution => {
+      return solution.every(solutionTile => {
+        return currentShapeTiles.includes(solutionTile);
+      })
+    })) {
+      return currentTurn;
+    } else if (tiles.every(t => t !== '')) {
+      return 'no winner';
+    } else {
+      return '';
     }
-    else if (tiles.every(t => t !== '')) {
+  }
+
+  const setWinner = winner => {
+    if (VALID_SHAPES.includes(winner)) {
+      setStatus(`The winner is ${currentTurn} !`);
+    } else {
       setStatus('It\'s a draw.');
     }
   }
 
-  const getTilesWithShape = (shape=currentTurn) => {
-    let out = [];
-    tiles.map((tile, key) => { 
-      if (tile === shape) out.push(key);
-    });
-    return out;
+
+  const getTileIndicesWithShape = shape => {
+    if (!VALID_SHAPES.includes(shape)) return false;
+
+    let tilesWithId = tiles.map((tile, id) => [tile, id]);
+    let filteredTiles = tilesWithId.filter(tile => tile[0] === shape);
+    let filteredTileIndices = filteredTiles.map(tile => tile[1]);
+
+    return filteredTileIndices;
   }
+
+
 
   const resetGame = () => {
-    setTiles(initialTiles);
-    setCurrentTurn(initialTurn);
-    setStatus(initialStatus);
+    setTiles(INITIAL_TILES);
+    setCurrentTurn(VALID_SHAPES[Math.floor(Math.random() * VALID_SHAPES.length)]);
+    setStatus(INITIAL_STATUS);
+    setGameOver(false);
   }
 
+
+
+
   return (
-    <div>
-      <h3>Current Turn: {currentTurn}</h3>
-      <h5>Status: {status}</h5>
+    <div className="game-container">
+
+      <h2>Tic Tac Toe</h2>
+      <h5>Current Turn: {currentTurn}</h5>
+      <h5> &nbsp; { status !== '' && status } &nbsp; </h5>
+
       <div class="grid">
         { tiles.map((tile, key) => {
           return <GameTile
             key={key}
             id={key}
             inputShape={tile}
-            clickCallback={playTurn}
+            clickCallback={setCheckedTile}
+            disabled={gameOver}
           /> 
         }) }
       </div>
+
       <button onClick={() => resetGame()}>Reset</button>
     </div>
   );
--- src/GameTile.js
+++ src/GameTile.js
@@ -1,21 +1,41 @@
 import React from 'react';
 import './styles.scss';
 
-const GameTile = ({ id, inputShape, clickCallback }) => {
+const GameTile = ({ id, inputShape, clickCallback, disabled }) => {
 
-  let main = <span />
+	const SHAPE_TO_ICON = {
+		'o': 'panorama_fish_eye',
+		'x': 'clear',
+	}
 
-  if (inputShape === 'o') {
-    main = <i class="material-icons">panorama_fish_eye</i>
-  } else if (inputShape === 'x') {
-    main = <i class="material-icons">clear</i>
-  } 
-  return (
-    <div
-    	className="tile"
-    	onClick={() => clickCallback(id)}
-    >{ main }</div>
-  );
+	const icon = <i class="material-icons">{ SHAPE_TO_ICON[inputShape] }</i>
+	const iconContent = inputShape ? icon : <span />;
+
+
+ 	let output;
+  	if (disabled) {
+  		output = <div className="tile disabled">
+    		{ iconContent }
+    	</div>
+
+  	} else {
+  		if (inputShape) {
+  			output = <div className="tile filled">
+  				{ iconContent}
+  			</div>
+
+  		} else {
+  			output = <div
+	    		className="tile"
+	    		onClick={() => clickCallback(id)}
+	    		>
+	    		{ iconContent }
+	    	</div>
+  		}
+  	}
+  	
+  	
+  	return  output;
 }
 
 export default GameTile;
